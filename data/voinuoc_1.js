56eb391141d9dc1d45bb83f7df5e551c0358d46d
webgistree-tdmu <144410364+webgistree-tdmu@users.noreply.github.com>
2023-09-28 09:21:07 +0700

    Add files via upload

---
+++ data/voinuoc_1.js
@@ -0,0 +1,27 @@
+var json_voinuoc_1 = {
+"type": "FeatureCollection",
+"name": "voinuoc_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "1.0", "X": "10.980435", "Y": "106.674219", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674219, 10.980435, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "2.0", "X": "10.980426", "Y": "106.674327", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674327, 10.980426, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "3.0", "X": "10.980487", "Y": "106.674678", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674678, 10.980487, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "4.0", "X": "10.980507", "Y": "106.674775", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674775, 10.980507, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "5.0", "X": "10.980792", "Y": "106.674867", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674867, 10.980792, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "6.0", "X": "10.980773", "Y": "106.674724", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674724, 10.980773, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "7.0", "X": "10.98076", "Y": "106.674676", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674676, 10.98076, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "8.0", "X": "10.980748", "Y": "106.67459", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.67459, 10.980748, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "9.0", "X": "10.980734", "Y": "106.674503", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674503, 10.980734, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "10.0", "X": "10.980692", "Y": "106.674305", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674305, 10.980692, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "11.0", "X": "10.980671", "Y": "106.674178", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674178, 10.980671, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "12.0", "X": "10.981309", "Y": "106.674127", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "2 - 3 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674127, 10.981309, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "13.0", "X": "10.981353", "Y": "106.674401", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "2 - 3 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674401, 10.981353, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "14.0", "X": "10.981897", "Y": "106.674574", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674574, 10.981897, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "15.0", "X": "10.980595", "Y": "106.673605", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.673605, 10.980595, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "16.0", "X": "10.979028", "Y": "106.674508", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "Khong su dung thuong xuyen" }, "geometry": { "type": "Point", "coordinates": [ 106.674508, 10.979028, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "17.0", "X": "10.979006", "Y": "106.674627", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "Khong su dung thuong xuyen" }, "geometry": { "type": "Point", "coordinates": [ 106.674627, 10.979006, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "18.0", "X": "10.97897", "Y": "106.674831", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.674831, 10.97897, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "19.0", "X": "10.979019", "Y": "106.675245", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.675245, 10.979019, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "20.0", "X": "10.97906", "Y": "106.67545", "Loai_voi_t": "Ong nuoc PVC 27mm", "Chu_ki_tuo": "1 - 2 lan\/ngay" }, "geometry": { "type": "Point", "coordinates": [ 106.67545, 10.97906, 0.0 ] } }
+]
+}
